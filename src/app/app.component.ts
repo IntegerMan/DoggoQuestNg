e1d4a6c6e25938c9c287d594b7f60524384c087d
Matt Eland <Matt.Eland@GMail.com>
2020-02-04 01:16:52 -0500

    Styling work on main UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'doggo-quest';
+export class AppComponent implements OnInit {
+
+  constructor() {
+    this.onResize();
+  }
+
+  @ViewChild('scrollMe', { read: ViewContainerRef, static: true }) scrollContainer;
+
+  public StoryCardHeight: number;
+
+  public onResize(): void {
+    this.StoryCardHeight = window.innerHeight - 200;
+  }
+
+  public ngOnInit(): void {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+  }
 }

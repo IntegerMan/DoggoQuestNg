494dfe50783f67a8a277bbff501adc1942e0fe21
Matt Eland <Matt.Eland@GMail.com>
2020-02-04 21:09:52 -0500

    Components now take in text from other components

--- src/app/player-command/player-command.component.ts
+++ src/app/player-command/player-command.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-player-command',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player-command.component.scss']
 })
 export class PlayerCommandComponent implements OnInit {
+
+  @Input()
+  public Text: string;
 
   constructor() { }
 
--- src/app/story-text/story-text.component.ts
+++ src/app/story-text/story-text.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-story-text',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./story-text.component.scss']
 })
 export class StoryTextComponent implements OnInit {
+
+  @Input()
+  public Text: string;
 
   constructor() { }
 

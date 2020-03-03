77d0f7c8820b0faf4eeb522c875c1057a23b8f2c
Matt Eland <Matt.Eland@GMail.com>
2020-03-02 23:57:17 -0500

    Improved string searching

--- src/app/EngineTest.ts
+++ src/app/EngineTest.ts
@@ -1,22 +1,17 @@
-import {LoggingService} from './logging.service';
+import {StoryEntryType} from '../Model/StoryEntryType';
 import {ParserService} from './parser.service';
 import {StoryService} from './story.service';
 import {VerbService} from './verb.service';
 import {WorldService} from './world.service';
 
-export class EngineTestHarness {
-  public static commandShouldResultInText(text: string, resultText: string): string | null {
-    const logger = new LoggingService();
+export class EngineTest {
+  public static getResponse(text: string): string {
     const parser = new ParserService(null);
     const verbs = new VerbService();
-    const world = new WorldService(logger);
+    const world = new WorldService();
     const service = new StoryService(parser, verbs, world);
 
-    const results = service.handlePlayerInput(text);
-
-    if (results.filter(r => r.Text && r.Text.indexOf(resultText))) {
-      return null;
-    }
+    const results = service.handlePlayerInput(text).filter(r => r.EntryType !== StoryEntryType.PlayerCommand);
 
     return results.map(r => r.Text).join('\r\n');
   }
